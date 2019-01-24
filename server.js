25c7ace65d31e7cb82adfdd35b30d906bd891a00
Jr Cenina <44345078+jayAardvark@users.noreply.github.com>
2019-01-23 23:37:19 -0800

    started api routes

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
+const newUser = require("./routes/api/newUser");
+
 app.get("/", (req, res) => res.send("Hello!"));
+
+app.use("/api/exercise/new-user", newUser);
 
 const port = process.env.PORT || 5000;
 
