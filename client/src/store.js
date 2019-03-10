579f710924882128c9790141a07d501d2ce55d7c
Jr Cenina <44345078+jayAardvark@users.noreply.github.com>
2019-03-09 18:40:08 -0800

    deploy redux comment out

--- client/src/store.js
+++ client/src/store.js
@@ -13,9 +13,9 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
+    applyMiddleware(...middleware)
     //below is from redux dev tool documentation
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
